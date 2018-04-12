f1350a8d46259c76eacf50c35b41a81c64f5412e
marcrauch <rauchm@student.ethz.ch>
2018-04-12 16:44:05 +0200

    Added basic bluetooth scanning capabilities :)

--- app/src/main/java/tim_mueller/ps2018/MainActivity.java
+++ app/src/main/java/tim_mueller/ps2018/MainActivity.java
@@ -1,11 +1,13 @@
 package tim_mueller.ps2018;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,41 +20,34 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
     private ArrayList<String> mDeviceList = new ArrayList<String>();
-    mReceiver mReceiveru = new mReceiver();
-
-    private class mReceiver extends BroadcastReceiver {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            Log.i("BT", "schölölölölöl!");
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                BluetoothDevice device = intent
-                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                mDeviceList.add(device.getName() + " - " + device.getAddress());
-                Log.i("BT", device.getName() + "\n" + device.getAddress());
-                listView.setAdapter(new ArrayAdapter<String>(context,
-                        android.R.layout.simple_list_item_1, mDeviceList));
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         listView = (ListView) findViewById(R.id.listView);
 
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (!mBluetoothAdapter.isEnabled()){
             Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBT, 0xDEADBEEF);
-
+            startActivity(enableBT);
         }
+
         else Log.i("BT", "Bluetooth enabled!");
         if (mBluetoothAdapter.isDiscovering()) {
             mBluetoothAdapter.cancelDiscovery();
             Log.i("BT", "Stopped previous discoveries!");
         }
+
+        int MY_PERMISSIONS_REQUEST_ACCESS_BLUETOOTH = 1;
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.BLUETOOTH},
+                MY_PERMISSIONS_REQUEST_ACCESS_BLUETOOTH);
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
         try {
             mBluetoothAdapter.startDiscovery();
             Log.i("BT", "discovery started!");
@@ -60,17 +55,32 @@ public class MainActivity extends AppCompatActivity {
         catch (Exception e) {
             Log.i("BT", "couldn't start discovery!");
         }
-        mReceiver mReceiveru = new mReceiver();
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(mReceiveru, filter);
         System.out.println(mDeviceList);
 
     }
 
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                if(deviceName!= null) mDeviceList.add(deviceName);
+                ArrayAdapter adapter = new ArrayAdapter<String>(context,android.R.layout.simple_list_item_1, mDeviceList);
+                listView.setAdapter(adapter);
+                if(deviceName != null) Log.i("BT", deviceName);
+            }
+        }
+    };
+
+
 
     @Override
     protected void onDestroy() {
-        unregisterReceiver(mReceiveru);
+        unregisterReceiver(mReceiver);
         super.onDestroy();
     }
 
