e095557e1af02daa6f1a7b7c5d6443e67c327fae
Marc Rauch <marcrauch@public-docking-etx-0620.ethz.ch>
2018-04-12 13:23:09 +0200

    arduino sketch added

---
+++ sketch_mar24a/sketch_mar24a.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(115200);  //initial the Serial
+}
+
+void loop() {
+  String test;
+  if (Serial.available())  {
+    test = Serial.readString();//send what has been received
+    Serial.print(test);//send what has been received
+  }
+}
